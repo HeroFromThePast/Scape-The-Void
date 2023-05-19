d0a1e4735adb9012ca955d170eb384f443ac012b
Juan José Pérez Sánchez <juan.jperezs001@gmail.com>
2023-05-18 22:43:59 -0500

    Scriptcito xd

--- Assets/Scripts/DoorManager.cs
+++ Assets/Scripts/DoorManager.cs
@@ -5,11 +5,42 @@ using UnityEngine;
 public class DoorManager : MonoBehaviour
 {
     [SerializeField] Animator animDoor1;
-    [SerializeField] Animator animDoor2;
-    [SerializeField] Animator animDoor3;
 
     [SerializeField] SimonSays simonSaysScript;
-    
 
+    public int bateries = 0;
+    bool bateriesReady = false;
+
+    public int wheels = 0;
+    bool wheelsReady = false;
+
+    public void ConnectedBaterie()
+    {
+        bateries++;
+    }
+
+    public void FitWheel()
+    {
+        wheels++;
+    }
+
+    private void Update()
+    {
+        if (simonSaysScript.ganar == true)
+        {
+            animDoor1.SetBool("Completed", true);
+        }
+
+        if (bateries == 3)
+        {
+            bateriesReady = true;
+        }
+
+        if (wheels == 4)
+        {
+            wheelsReady = true;
+        }
+
+    }
 
 }
