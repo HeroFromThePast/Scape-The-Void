9be1aeb482ecc19322fd4f8e8e851bd048b96eed
Juan José Pérez Sánchez <juan.jperezs001@gmail.com>
2023-05-18 21:48:58 -0500

    Prefabs puertas

---
+++ Assets/Scripts 1/DoorManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorManager : MonoBehaviour
+{
+    [SerializeField] Animator animDoor1;
+    [SerializeField] Animator animDoor2;
+    [SerializeField] Animator animDoor3;
+
+    [SerializeField] SimonSays simonSaysScript;
+    
+
+
+}
